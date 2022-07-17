f03a84fb6e14e81be6368ab8efe04a5679cf58cc
Vryuss <tristan.c.meyers@gmail.com>
2022-07-17 13:20:16 -0400

    Added button to change Grid Size and clear grid

--- script.js
+++ script.js
@@ -1,17 +1,39 @@
 const canvas = document.querySelector('.canvas')
+var root = document.querySelector(':root');
+var rootStyles = getComputedStyle(root);
 
-for(let i = 0; i < 15; i++){
-	for(let j = 0; j < 16; j++){
+function drawTiles(){
+var size = rootStyles.getPropertyValue('--gridsize');
+
+for(let i = 0; i < size; i++){
+	for(let j = 0; j < size; j++){
 		const tile = document.createElement('div')
 		tile.classList.add('tile')
 		canvas.appendChild(tile)
 	}
 }
+}
+drawTiles();
 
-const tiles = document.querySelectorAll('.tile');
-
+let tiles = document.querySelectorAll('.tile');
+function listenTiles(){
+tiles = document.querySelectorAll('.tile');
 tiles.forEach((tile) => {
 	tile.addEventListener('mouseover', () => {
 		tile.classList.add('draw');
 	})
+})
+}
+listenTiles();
+
+const button = document.querySelector('.sizepromt');
+
+button.addEventListener('click', () => {
+	let grid = prompt("Enter the new grid size");
+	root.style.setProperty('--gridsize', grid);
+	tiles.forEach(tile => {
+		tile.remove();			
+	})
+	drawTiles();
+	listenTiles();
 })
